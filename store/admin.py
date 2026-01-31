25949146f93933a70e5dbfa4bcaed4d32e77f1d8
Ganika <ganika3011@gmail.com>
2026-01-31 12:46:38 +0530

    store and carts functionality

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product,Variation
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
@@ -8,5 +8,12 @@ class ProductAdmin(admin.ModelAdmin):
     list_editable= ('is_available','stock')
     search_fields= ('product_name',)
     list_filter= ('is_available','created_date','modified_date')
+
+@admin.register(Variation)
+class VariationAdmin(admin.ModelAdmin):
+    list_display= ('product','variation_category','variation_value','is_active','created_date')
+    list_editable= ('is_active',)
+    list_filter= ('variation_category','is_active')
+    search_fields= ('product__product_name',)
 
 # Register your models here.
--- store/models.py
+++ store/models.py
@@ -22,3 +22,27 @@ class Product(models.Model):
         return reverse('product_detail', args=[self.category.slug, self.slug])
     def __str__(self):
         return self.product_name
+
+choices= (
+        ('color','color'),
+        ('size','size'),
+    )
+class VariationManager(models.Manager):
+    def colors(self):
+        return super(VariationManager, self).filter(variation_category='color', is_active=True)
+    def sizes(self):
+        return super(VariationManager, self).filter(variation_category='size', is_active=True)    
+
+class Variation(models.Model):
+    product= models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category= models.CharField(max_length=100,choices=choices)
+    variation_value= models.CharField(max_length=100)
+    is_active= models.BooleanField(default=True)
+    created_date= models.DateTimeField(auto_now=True)
+
+    objects= models.Manager()
+    objects= VariationManager()
+
+    def __str__(self):
+        return self.variation_value
+
